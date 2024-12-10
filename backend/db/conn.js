a899e7fdc393dc8dfb4f125237a5471bd953d5f2
Eduardoss45 <temporariac432@gmail.com>
2024-12-10 20:52:39 -0300

    backend finalizado

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -6,7 +6,7 @@ mongoose.set("strictQuery", true);
 
 async function main() {
   await mongoose.connect(
-    `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@ac-c0pfbt6-shard-00-00.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-01.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-02.xlmhjcx.mongodb.net:27017/?ssl=true&replicaSet=atlas-xw5ill-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-shard-00-00.qbiqh.mongodb.net:27017,cluster0-shard-00-01.qbiqh.mongodb.net:27017,cluster0-shard-00-02.qbiqh.mongodb.net:27017/?ssl=true&replicaSet=atlas-10n4ir-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
   );
 
   console.log("Conectado com sucesso!");
